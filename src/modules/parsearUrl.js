7825bc59603621645a8679aa1014111f2547a3aa
BRUNO NAHUEL MARASI <48113202@ort>
2025-04-07 11:45:03 -0300

    terminamos

--- src/modules/parsearUrl.js
+++ src/modules/parsearUrl.js
@@ -2,14 +2,23 @@
 const parsearUrl = (url) => {
     const parsedUrl = new URL(url);
     const params = {};
-    parsedUrl.searchParams.forEach((value, key) => {
-        params[key] = value;
+
+
+
+    parsedUrl.searchParams.forEach((valor, i) => {
+        params[i] = valor;
     })
 
-    return {
+    let objetoUrl = {
         host: `${parsedUrl.protocol}//${parsedUrl.host}`,
         pathname: parsedUrl.pathname,
         parametros: params
+        
+    }
+
+    return {
+        objetoUrl
     }
 }
+
 module.exports = parsearUrl
